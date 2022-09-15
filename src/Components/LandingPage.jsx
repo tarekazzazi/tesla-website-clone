1935f2596102b6f8a5dd7faecdccca5d66187609
Tarek Azzazi <azzazi.tarek@gmail.com>
2022-09-15 14:54:00 -0500

    smooth scroll working

--- src/Components/LandingPage.jsx
+++ src/Components/LandingPage.jsx
@@ -13,52 +13,63 @@ function LandingPage() {
 
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <nav className="topnav">
-          <h5>Model S</h5>
-          <h5>Model 3</h5>
-          <h5>Model X</h5>
-          <h5>Model Y</h5>
-          <h5>Solar Roof</h5>
-          <h5>Solar Panels</h5>
-        </nav>
-      </header>
-      <br />
-      <br />
-      <br />
-      <div className="secondaryHeader App-header">
-        <h1 id="CarTitle">Place Car Name here</h1>
-
-        <p>
-          Order online for{" "}
-          <a href="https://www.w3schools.com">Touchless Delivery</a>
-        </p>
-
-        <br />
-        <div className="Btns">
-          <button id="customOrder"> Custom Order </button>
-          <button id="Inventory"> Existing Inventory</button>
+      <div className="scroll-container">
+        <div className="section">
+          <header className="App-header">
+            <img src={logo} className="App-logo" alt="logo" />
+            <nav className="topnav">
+              <h5>Model S</h5>
+              <h5>Model 3</h5>
+              <h5>Model X</h5>
+              <h5>Model Y</h5>
+              <h5>Solar Roof</h5>
+              <h5>Solar Panels</h5>
+            </nav>
+          </header>
           <br />
+          <br />
+          <br />
+          <div className="secondaryHeader App-header">
+            <h1 id="CarTitle">Place Car Name here</h1>
 
-          <img src={"./assets/down-arrow.png"} id="downArrow" width="25px" />
+            <p>
+              Order online for{" "}
+              <a href="https://www.w3schools.com">Touchless Delivery</a>
+            </p>
+
+            <br />
+            <div className="Btns">
+              <button id="customOrder"> Custom Order </button>
+              <button id="Inventory"> Existing Inventory</button>
+              <br />
+
+              <img
+                src={"./assets/down-arrow.png"}
+                id="downArrow"
+                width="25px"
+              />
+            </div>
+          </div>
         </div>
-      </div>
 
-      <div>
-        <img src="/assets/M3-Homepage-D.jpg" className="img" />
-      </div>
-
-      <div>
-        <img src="/assets/model-y_R1@2.jpg" className="img" />
-      </div>
-
-      <div>
-        <img src="/assets/ms-main-hero-desktop.jpg" className="img" />
-      </div>
-
-      <div>
-        <img src="/assets/MX-Hero-Desktop.jpg" className="img" />
+        <div className="section">
+          <img src="/assets/M3-Homepage-D.jpg" className="img" />
+        </div>
+        <div className="section">
+          <img src="/assets/model-y_R1@2.jpg" className="img" />
+        </div>
+        <div className="section">
+          <img src="/assets/ms-main-hero-desktop.jpg" className="img" />
+        </div>
+        <div className="section">
+          <img src="/assets/MX-Hero-Desktop.jpg" className="img" />
+        </div>
+        <div className="section">
+          <img src="/assets/solar-panel-desktop.jpg" className="img" />
+        </div>
+        <div className="section">
+          <img src="/assets/sr-storm-desktop.png" className="img" />
+        </div>
       </div>
     </div>
   );
