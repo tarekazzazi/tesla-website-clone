8ee1e33ebd54e22738692051a971f9f7e7d63563
Tarek Azzazi <azzazi.tarek@gmail.com>
2022-09-16 20:17:40 -0500

    handle scroll function created

--- src/Components/LandingPage.jsx
+++ src/Components/LandingPage.jsx
@@ -1,20 +1,42 @@
 import "./LandingPage.css";
 import logo from "../../assets/tesla.svg";
-import { Link } from "react-router-dom";
+
 function LandingPage() {
-  let cars = [
-    "M3-Homepage-D.jpg",
-    "model-y_R1@2.jpg",
-    "ms-main-hero-desktop.jpg",
-    "MX-Hero-Desktop.jpg",
-    "solar-panel-desktop.jpg",
-    "sr-storm-desktop.png",
+  const model = [
+    "Model 3",
+    "Model Y",
+    "Model S",
+    "Model X",
+    "Solar Panels",
+    "Solar Roof",
   ];
+
+  const handleScroll = (event) => {
+    // console.log("scrollTop: ", event.currentTarget.scrollTop);
+    let type;
+    let x = event.currentTarget.scrollTop;
+    console.log(x);
+    // console.log("Window Scroll Y", window.scrollY);
+    if (x < 1000) {
+      type = "telsa model 3";
+      console.log("hello");
+    } else if (x < 1360) {
+      console.log("WHOA");
+    } else if (x < 2200) {
+      console.log("No way");
+    } else {
+      console.log("NOPE");
+    }
+    // console.log("type is this", type);
+    return;
+  };
+
+  // console.log("type is this", type);
 
   return (
     <div className="App">
-      <div className="scroll-container">
-        <div className="section App-header">
+      <div className="scroll-container" onScroll={handleScroll}>
+        <div className=" App-header">
           <header className="App-header">
             <img src={logo} className="App-logo" alt="logo" />
             <nav className="topnav">
@@ -30,7 +52,7 @@ function LandingPage() {
           <br />
           <br />
           <div className="secondaryHeader App-header">
-            <h1 id="CarTitle">Place Car Name here</h1>
+            <h1 id="CarTitle">{}</h1>
 
             <p>
               Order online for{" "}
@@ -43,31 +65,35 @@ function LandingPage() {
               <button id="Inventory"> Existing Inventory</button>
               <br />
 
-              <img
-                src={"./assets/down-arrow.png"}
-                id="downArrow"
-                width="25px"
-              />
+              <div className="main" id="section">
+                <a href="#1">
+                  <img
+                    src={"./assets/down-arrow.png"}
+                    id="downArrow"
+                    width="25px"
+                  />
+                </a>
+              </div>
             </div>
           </div>
         </div>
 
-        <div className="section">
+        <div className="section" id="0">
           <img src="/assets/M3-Homepage-D.jpg" className="img" />
         </div>
-        <div className="section">
+        <div className="section" id="1">
           <img src="/assets/model-y_R1@2.jpg" className="img" />
         </div>
-        <div className="section">
+        <div className="section" id="2">
           <img src="/assets/ms-main-hero-desktop.jpg" className="img" />
         </div>
-        <div className="section">
+        <div className="section" id="3">
           <img src="/assets/MX-Hero-Desktop.jpg" className="img" />
         </div>
-        <div className="section">
+        <div className="section" id="4">
           <img src="/assets/solar-panel-desktop.jpg" className="img" />
         </div>
-        <div className="section">
+        <div className="section" id="5">
           <img src="/assets/sr-storm-desktop.png" className="img" />
         </div>
       </div>
